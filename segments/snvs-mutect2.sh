dd6e029e64a361684752a0d3e649fa11e0a7913b
Farheen Shah <sfar0315@gmail.com>
2022-12-14 13:03:38 -0500

    Update snvs-mutect2.sh

--- segments/snvs-mutect2.sh
+++ segments/snvs-mutect2.sh
@@ -3,6 +3,8 @@
 
 # call variants with Mutect2 (2.1 part of GATK 4)
 
+# ML-Week of September 12, removed all mention of Sample Normal
+# FS-November 4th, edited Mary's mutect 2 script to make it similar to Terra's CHIP script
 
 # script filename
 script_path="${BASH_SOURCE[0]}"
@@ -11,9 +13,9 @@ segment_name=${script_name/%.sh/}
 echo -e "\n ========== SEGMENT: $segment_name ========== \n" >&2
 
 # check for correct number of arguments
-if [ ! $# == 6 ] ; then
+if [ ! $# == 4 ] ; then
 	echo -e "\n $script_name ERROR: WRONG NUMBER OF ARGUMENTS SUPPLIED \n" >&2
-	echo -e "\n USAGE: $script_name project_dir tumor_sample_name tumor_bam normal_sample_name normal_bam threads \n" >&2
+	echo -e "\n USAGE: $script_name project_dir tumor_sample_name tumor_bam threads \n" >&2
 	if [ $# -gt 0 ] ; then echo -e "\n ARGS: $* \n" >&2 ; fi
 	exit 1
 fi
@@ -22,9 +24,7 @@ fi
 proj_dir=$(readlink -f "$1")
 sample_t=$2
 bam_t=$3
-sample_n=$4
-bam_n=$5
-threads=$6
+threads=$4
 
 
 #########################
@@ -39,11 +39,6 @@ fi
 
 if [ ! -s "$bam_t" ] ; then
 	echo -e "\n $script_name ERROR: BAM $bam_t DOES NOT EXIST \n" >&2
-	exit 1
-fi
-
-if [ ! -s "$bam_n" ] ; then
-	echo -e "\n $script_name ERROR: BAM $bam_n DOES NOT EXIST \n" >&2
 	exit 1
 fi
 
@@ -77,11 +72,11 @@ fi
 
 # settings and files
 
-sample="${sample_t}:${sample_n}"
+sample="${sample_t}"
 
 summary_dir="${proj_dir}/summary"
 mkdir -p "$summary_dir"
-contamination_csv="${summary_dir}/${sample_t}-${sample_n}.VCF-Mutect2-contamination.csv"
+contamination_csv="${summary_dir}/${sample_t}.VCF-Mutect2-contamination.csv"
 
 vcf_dir="${proj_dir}/VCF-Mutect2"
 mkdir -p "$vcf_dir"
@@ -89,17 +84,24 @@ mkdir -p "$vcf_dir"
 mutect_logs_dir="${proj_dir}/logs-${segment_name}"
 mkdir -p "$mutect_logs_dir"
 
-vcf_unfiltered="${mutect_logs_dir}/${sample_t}-${sample_n}.unfiltered.vcf"
+vcf_unfiltered="${mutect_logs_dir}/${sample_t}.unfiltered.vcf"
 idx_unfiltered="${vcf_unfiltered}.idx"
 
-vcf_filtered="${vcf_dir}/${sample_t}-${sample_n}.original.vcf.gz"
+vcf_filtered="${vcf_dir}/${sample_t}.original.vcf.gz"
 idx_filtered="${vcf_filtered}.tbi"
 
-vcf_fixed="${vcf_dir}/${sample_t}-${sample_n}.vcf"
+vcf_fixed="${vcf_dir}/${sample_t}.vcf"
 
-pileup_table_t="${mutect_logs_dir}/${sample_t}-${sample_n}.t.pileup.txt"
-pileup_table_n="${mutect_logs_dir}/${sample_t}-${sample_n}.n.pileup.txt"
-contamination_table="${mutect_logs_dir}/${sample_t}-${sample_n}.contamination.txt"
+pileup_table_t="${mutect_logs_dir}/${sample_t}.t.pileup.txt"
+contamination_table="${mutect_logs_dir}/${sample_t}.contamination.txt"
+stats="${mutect_logs_dir}/${sample_t}.unfiltered.vcf.stats"
+bam_output="${mutect_logs_dir}/${sample_t}.bamout.bam"
+f1r2_counts="${mutect_logs_dir}/${sample_t}.f1r2.tar.gz"
+artifact_prior="${mutect_logs_dir}/${sample_t}.artifact_prior.tar.gz"
+segment_table="${mutect_logs_dir}/${sample_t}.segments.table"
+
+# FS 11/04/2022 Included stats in settings and files
+
 
 # annotation command (next segment)
 annot_cmd="bash ${code_dir}/segments/annot-annovar.sh $proj_dir $sample $vcf_fixed"
@@ -164,15 +166,13 @@ fi
 module add python/cpu/3.6.5
 
 # command
-gatk_bin="/gpfs/data/igorlab/software/GenomeAnalysisTK/gatk-4.2.4.0/gatk"
+gatk_bin="${proj_dir}/gatk-4.1.6.0/gatk"
 
 echo
 echo " * GATK: $(readlink -f $gatk_bin) "
 echo " * GATK version: $($gatk_bin Mutect2 --version 2>&1 | grep "v4") "
 echo " * sample T : $sample_t "
 echo " * BAM T : $bam_t "
-echo " * sample N : $sample_n "
-echo " * BAM N : $bam_n "
 echo " * intervals BED: $bed "
 echo " * VCF unfiltered: $vcf_unfiltered "
 echo
@@ -188,20 +188,23 @@ $gatk_bin --java-options \"-Xms8G -Xmx8G\" Mutect2 \
 --native-pair-hmm-threads $threads \
 --reference $ref_fasta \
 $germline_resource_arg \
---dont-use-soft-clipped-bases \
---max-reads-per-alignment-start 100 \
 --intervals $bed \
---interval-padding 10 \
 --input $bam_t \
---input $bam_n \
 --tumor-sample $sample_t \
---normal-sample $sample_n \
+--panel-of-normals ${proj_dir}/1000g_pon.hg38.vcf.gz \
+--bam-output $bam_output \
+--f1r2-tar-gz $f1r2_counts \
+--downsampling-stride 20 \
+--max-suspicious-reads-per-alignment-start 6 \
+--max-reads-per-alignment-start 6 \
 --output $vcf_unfiltered \
 "
 echo -e "\n CMD: $mutect_cmd \n"
 eval "$mutect_cmd"
 
-
+# ML—Week of September 12, changed to panel of normal comparison (from comparison with Sample Normal)
+# FS 11/04/2022 Removed --dont-use-soft-clipped-bases argument, removed --max-reads-per-alignment-start 100, removed interval padding
+# FS 11/04/2022 Added --bam-output $bam_output, --f1r2-tar-gz 
 #########################
 
 
@@ -230,7 +233,7 @@ fi
 # GetPileupSummaries: tabulates read counts that support ref and alt alleles for the sites in the resource
 # CalculateContamination: calculate the fraction of reads coming from cross-sample contamination
 
-if [ -n "$pileup_variants" ] ; then
+# if [ -n "$pileup_variants" ] ; then
 
 	# tabulates pileup metrics for T
 	pileup_t_cmd="
@@ -238,170 +241,169 @@ if [ -n "$pileup_variants" ] ; then
 	--verbosity WARNING \
 	--variant $pileup_variants \
 	--intervals $bed \
-	--interval-padding 100 \
+  	--reference $ref_fasta \
 	--input $bam_t \
 	--output $pileup_table_t \
 	"
 	echo -e "\n CMD: $pileup_t_cmd \n"
 	eval "$pileup_t_cmd"
 
-	# tabulates pileup metrics for N
-	pileup_n_cmd="
-	$gatk_bin --java-options \"-Xms8G -Xms8G\" GetPileupSummaries \
-	--verbosity WARNING \
-	--variant $pileup_variants \
-	--intervals $bed \
-	--interval-padding 100 \
-	--input $bam_n \
-	--output $pileup_table_n \
+# FS 11042022 Removed --interval-padding 100 from GetPileupSummaries
+# FS 11042022 Added --reference $ref_fasta
+#
+# LearnReadOrientationModel - Learn the prior probability of read orientation artifact from the output of CollectF1R2Counts of Mutect2
+	 learn_read_orientation_model_t_cmd="
+	 $gatk_bin --java-options \"-Xms8G -Xms8G\" LearnReadOrientationModel \
+	 --verbosity WARNING \
+	 --input $f1r2_counts \
+	 --output $artifact_prior \
+	 "
+	 echo -e "\n CMD: $learn_read_orientation_model_t_cmd \n"
+  	 eval "$learn_read_orientation_model_t_cmd"
+
+# # #FS 11042022 Added LearnReadOrientationModel tool
+#
+# 	# the resulting table provides the fraction contamination, one line per sample
+ 	contamination_cmd="
+ 	$gatk_bin --java-options \"-Xms8G -Xms8G\" CalculateContamination \
+ 	--verbosity WARNING \
+ 	--input $pileup_table_t \
+ 	--output $contamination_table \
+	--tumor-segmentation $segment_table \
 	"
-	echo -e "\n CMD: $pileup_n_cmd \n"
-	eval "$pileup_n_cmd"
+ 	echo -e "\n CMD: $contamination_cmd \n"
+ 	eval "$contamination_cmd"
 
-	# the resulting table provides the fraction contamination, one line per sample
-	contamination_cmd="
-	$gatk_bin --java-options \"-Xms8G -Xms8G\" CalculateContamination \
-	--verbosity WARNING \
-	--input $pileup_table_t \
-	--matched-normal $pileup_table_n \
-	--output $contamination_table \
-	"
-	echo -e "\n CMD: $contamination_cmd \n"
-	eval "$contamination_cmd"
+# #FS 11042022 Added --tumor-segmentation $segment_table
 
-	# check if contamination table is present
-	if [ ! -s "$contamination_table" ] ; then
-		echo -e "\n $script_name ERROR: CONTAMINATION TABLE $contamination_table NOT GENERATED \n" >&2
-		# delete pileups since something went wrong and they might be corrupted
-		rm -fv "$pileup_table_t"
-		rm -fv "$pileup_table_n"
-		exit 1
-	fi
-
-	# set the parameter for next step
-	contamination_arg="--contamination-table $contamination_table"
-
-	# contamination summary
-	contamination_score=$(cat "$contamination_table" | grep -v 'contamination' | head -1 | cut -f 2)
-	echo "contamination original: $contamination_score"
-	contamination_score=$(printf "%.5f" "$contamination_score")
-	echo "contamination rounded: $contamination_score"
-	contamination_header="#SAMPLE,contamination"
-	echo "${contamination_header}" > "$contamination_csv"
-	echo "${sample},${contamination_score}" >> "$contamination_csv"
-	sleep 5
-	cat ${summary_dir}/*.VCF-Mutect2-contamination.csv | LC_ALL=C sort -t ',' -k1,1 | uniq > "${proj_dir}/summary.VCF-Mutect2-contamination.csv"
-
-fi
+#
+#
+# 	# check if contamination table is present
+ 	if [ ! -s "$contamination_table" ] ; then
+ 		echo -e "\n $script_name ERROR: CONTAMINATION TABLE $contamination_table NOT GENERATED \n" >&2
+ 		# delete pileups since something went wrong and they might be corrupted
+# 		rm -fv "$pileup_table_t"
+ 		exit 1
+ 	fi
+#
+# 	# set the parameter for next step
+ 	contamination_arg="--contamination-table $contamination_table"
+#
+# 	# contamination summary
+ 	contamination_score=$(cat "$contamination_table" | grep -v 'contamination' | head -1 | cut -f 2)
+ 	echo "contamination original: $contamination_score"
+ 	contamination_score=$(printf "%.5f" "$contamination_score")
+ 	echo "contamination rounded: $contamination_score"
+ 	contamination_header="#SAMPLE,contamination"
+ 	echo "${contamination_header}" > "$contamination_csv"
+ 	echo "${sample},${contamination_score}" >> "$contamination_csv"
+ 	sleep 5
+ 	cat ${summary_dir}/*.VCF-Mutect2-contamination.csv | LC_ALL=C sort -t ',' -k1,1 | uniq > "${proj_dir}/summary.VCF-Mutect2-contamination.csv"
 
 
-#########################
-
-
-# GATK FilterMutectCalls
-
+#
+#
+# #########################
+#
+#
+# # GATK FilterMutectCalls
+#
 echo
 echo " * VCF unfiltered: $vcf_unfiltered "
-echo " * VCF filtered: $vcf_filtered "
-echo
-
-# --unique-alt-read-count: filter a variant if fewer than this many unique reads supporting the alternate allele (0)
-
+ echo " * VCF filtered: $vcf_filtered "
+ echo
+#
+# # --unique-alt-read-count: filter a variant if fewer than this many unique reads supporting the alternate allele (0)
+# #ML-Week of September 12, changed the --unique-alt-read-count and --min-allele-fraction arguments (lowered threshold)
+#
 mutect_filter_cmd="
 $gatk_bin --java-options \"-Xms8G -Xms8G\" FilterMutectCalls \
 --verbosity WARNING \
 --reference $ref_fasta \
---unique-alt-read-count 5 \
 --variant $vcf_unfiltered \
 $contamination_arg \
+--tumor-segmentation $segment_table \
+--ob-priors $artifact_prior \
+--filtering-stats $stats \
 --output $vcf_filtered \
 "
-echo -e "\n CMD: $mutect_filter_cmd \n"
-eval "$mutect_filter_cmd"
+ echo -e "\n CMD: $mutect_filter_cmd \n"
+ eval "$mutect_filter_cmd"
 
 
-#########################
-
-
-# check that output generated
-
-# check if VCF file is present
-if [ ! -s "$vcf_filtered" ] ; then
-	echo -e "\n $script_name ERROR: VCF $vcf_filtered NOT GENERATED \n" >&2
-	exit 1
-fi
-
+# #########################
+#
+#
+# # check that output generated
+#
+ # check if VCF file is present
+ if [ ! -s "$vcf_filtered" ] ; then
+ 	echo -e "\n $script_name ERROR: VCF $vcf_filtered NOT GENERATED \n" >&2
+ 	exit 1
+ fi
+#
 # check if VCF index is present (should be present if VCF is complete)
-if [ ! -s "$idx_filtered" ] ; then
+ if [ ! -s "$idx_filtered" ] ; then
 	echo -e "\n $script_name ERROR: VCF IDX $idx_filtered NOT GENERATED \n" >&2
-	# delete VCF since something went wrong and it might be corrupted
-	rm -fv "$vcf_filtered"
-	exit 1
-fi
+ 	# delete VCF since something went wrong and it might be corrupted
+ 	rm -fv "$vcf_filtered"
+ 	exit 1
+ fi
 
 
-#########################
+# #########################
+#
+#
+ # adjust VCF for ANNOVAR compatibility (http://annovar.openbioinformatics.org/en/latest/articles/VCF/) module add samtools/1.9
+
+ echo
+ echo " * samtools: $(readlink -f $(which samtools)) "
+ echo " * samtools version: $(samtools --version | head -1) "
+ echo " * VCF filtered: $vcf_filtered "
+ echo " * VCF fixed: $vcf_fixed "
+ echo
+
+ # 1) keep header and only passing variants
+ # 2) split multi-allelic variants calls into separate lines (uses VCF 4.2 specification)
+ # 3) perform indel left-normalization (start position shifted to the left until it is no longer possible to do so)
+
+ fix_vcf_cmd="
+ zcat $vcf_filtered \
+ > $vcf_fixed
+ "
+ echo -e "\n CMD: $fix_vcf_cmd \n"
+ eval "$fix_vcf_cmd"
 
 
-# adjust VCF for ANNOVAR compatibility (http://annovar.openbioinformatics.org/en/latest/articles/VCF/)
-
-module add samtools/1.9
-
-echo
-echo " * samtools: $(readlink -f $(which samtools)) "
-echo " * samtools version: $(samtools --version | head -1) "
-echo " * VCF filtered: $vcf_filtered "
-echo " * VCF fixed: $vcf_fixed "
-echo
-
-# 1) keep header and only passing variants
-# 2) split multi-allelic variants calls into separate lines (uses VCF 4.2 specification)
-# 3) perform indel left-normalization (start position shifted to the left until it is no longer possible to do so)
-
-fix_vcf_cmd="
-zcat $vcf_filtered \
-| grep -E '^#|PASS' \
-| bcftools norm --multiallelics -both --output-type v - \
-| bcftools norm --fasta-ref $ref_fasta --output-type v - \
-> $vcf_fixed
-"
-echo -e "\n CMD: $fix_vcf_cmd \n"
-eval "$fix_vcf_cmd"
+ #########################
 
 
-#########################
+ # check that output generated
+
+ if [ ! -s "$vcf_fixed" ] ; then
+ 	echo -e "\n $script_name ERROR: VCF $vcf_fixed NOT GENERATED \n" >&2
+ 	exit 1
+ fi
 
 
-# check that output generated
-
-if [ ! -s "$vcf_fixed" ] ; then
-	echo -e "\n $script_name ERROR: VCF $vcf_fixed NOT GENERATED \n" >&2
-	exit 1
-fi
+ #########################
 
 
-#########################
+ # clean up
 
+# rm -fv "$vcf_unfiltered"
+# rm -fv "$idx_unfiltered"
+# rm -fv "$pileup_table_t"
 
-# clean up
-
-rm -fv "$vcf_unfiltered"
-rm -fv "$idx_unfiltered"
-rm -fv "$pileup_table_t"
-rm -fv "$pileup_table_n"
-
-
-#########################
+ #########################
 
 
 # annotate
 
-echo -e "\n CMD: $annot_cmd \n"
-($annot_cmd)
+ echo -e "\n CMD: $annot_cmd \n"
+ ($annot_cmd)
 
 
-#########################
-
-
+ #########################
 
 # end
